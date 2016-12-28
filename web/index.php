96efdbaeaa63dd2d41269395d4578e957147c34b
Luna Versity <mrgodzbrother@gmail.com>
2016-12-28 05:11:15 -0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,72 +1,57 @@
 <?php
-//START CONFIG------------------------
-    $DB_HOSTNAME = "localhost";
-    $DB_USERNAME = "admin";
-    $DB_PASSWORD = "password";
-    $DB_NAME = "generic_name";
-//END CONFIG--------------------------
-
-	$pdo = new PDO("mysql:host=$DB_HOSTNAME;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$result = array();
-	try {
-   		$result = $pdo->query("SELECT SteamID, BugReport FROM bug_report");
-    	$result = $result->fetchAll();
-	} catch (Exception $e) {
-    	$error = $e->getMessage();
-	}
+// Edit this --->
+$db_host = "127.0.0.1"; // db host (127.0.0.1 is localhost)
+$db_name = ""; // the name of the database
+$db_user = ""; // the username of the db account
+$db_pass = ""; // password of the db account
+// Edit this <---
+try{
+$host = "mysql:host=$db_host;dbname=$db_name";
+$pdo = new PDO($host, $db_user, $db_pass);
+}
+catch(PDOException $pdo_err){ ?>
+    <h1 style="color:#c10000;">Error Caught:</h1>
+    <h2><?= $pdo_err->getMessage() ?></h2>
+  <?php die();
+}
+// Use PDO instead of MySQLi please.
+$bug_report = $pdo->prepare("SELECT * FROM `bug_report`"); // Prepare all statements
+$bug_report->execute(); // Execute it
+$bug = $bug_report->fetchAll(PDO::FETCH_ASSOC); // Grab the data
 ?>
-
-<!DOCTYPE HTML>
-<html>
-	<head>
-    	<title>3FX Bug Reports</title>
-    	<meta charset="UTF-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/darkly/bootstrap.min.css">
-    	<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-
-	<body class="background-color">
-    	<div class="container">
-        	<div class="row">
-           		<div class="col-lg-10 col-lg-push-1">
-                	<?php
-                		if (isset($error)) {
-                    		echo "<div class='alert alert-danger'>$error</div>";
-                		} else {
-                	?>
-                    	<table class="table table-bordered">
-                        	<thead>
-                            	<tr>
-                                	<th>Reporter (SteamID)</th>
-                                	<th>Description</th>
-                            	</tr>
-                        	</thead>
-
-                        	<tbody>
-                            	<?php foreach ($result as $row): ?>
-                            	<tr>
-                            		<td>
-                            			<?php echo htmlspecialchars( $row["SteamID"],ENT_QUOTES, 'UTF-8'); ?>
-                            		</td>
-                            		<td>
-                            			<?php echo htmlspecialchars( $row["BugReport"],ENT_QUOTES, 'UTF-8'); ?>
-                            		</td>
-                            	</tr>
-                            	<?php endforeach; ?>
-                        	</tbody>
-                    	</table>
-
-                    	<?php 
-                    		} 
-                    	?>
-            	</div>
-        	</div>
-    	</div>
-	</body>
-    <footer>
-        &copy; <?php echo date("Y") ?> by <a href="https://github.com/SubjectAlpha">Subject_Alpha</a>
-    </footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bug Reports</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+<body>
+  <div class="center-page">
+    <div>
+      <tbody>
+      <table class="center-table">
+        <tr><td colspan="2" style="text-align:center;">Bug Reports</td></tr>
+        <tr>
+          <td>SteamID</td>
+          <td>Description</td>
+        </tr>
+  <?php foreach($bug as $bu=>$b): ?>
+    <tr>
+      <td>
+        <?php echo $b['SteamID']; ?>
+      </td>
+      <td>
+        <?php echo htmlspecialchars($b['BugReport']); ?>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+<span class="credits">&copy; 2016 Subject_Alpha - <a href="https://github.com/Lunaversitay/Bug-Reporter/tree/master/web" style="color:#fff;">Fork</a></span>
+  </body>
 </html>
